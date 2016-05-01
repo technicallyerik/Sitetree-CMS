d9d52eb770a039c80c2b722dde0c1b85db163923
Erik Weiss <erik@flashforwardlabs.com>
2016-05-01 17:00:27 -0500

    Committing routing http handler test code while it works

--- SiteTree.Web/Routing/CmsRoutingHttpHandler.cs
+++ SiteTree.Web/Routing/CmsRoutingHttpHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SiteTree.Web.Routing
@@ -8,9 +9,20 @@ namespace SiteTree.Web.Routing
     {
         public void ProcessRequest(HttpContext context)
         {
-            var request = context.Request;
 
-            context.Response.Write("router");
+
+            var request = context.Request;
+            var requestContext = request.RequestContext;
+            var path = (string)requestContext.RouteData.Values["path"] ?? string.Empty;
+            var pathParts = path.Split('/');
+
+            var controllerFactory = ControllerBuilder.Current.GetControllerFactory();
+            var controller = controllerFactory.CreateController(requestContext, "Home");
+            requestContext.RouteData.Values.Add("controller", "Home");
+            requestContext.RouteData.Values.Add("action", "Index");
+            controller.Execute(requestContext);
+
+            //context.Response.Write("router");
         }
 
         public bool IsReusable => true;
